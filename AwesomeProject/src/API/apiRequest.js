d4c22df4197e238f3acd7f83da29aaa669103265
TatMinhNguyen <nguyentatminh2k2@gmail.com>
2023-10-25 13:36:08 +0700

    [update edit, delete students]

--- AwesomeProject/src/API/apiRequest.js
+++ AwesomeProject/src/API/apiRequest.js
@@ -1,12 +1,94 @@
-import axios from "axios";
-
-const baseUrl = "http://localhost:4000";
+const baseUrl = "http://10.90.169.92:4000";
 
 export const createStudent = async (user) => {
     try {
-        await axios.post(`${baseUrl}/add `, user);
-        console.log('User created successfully');
+        const response = await fetch(`${baseUrl}/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+
+        if (response.ok) {
+            console.log('User created successfully');
+        } else {
+            // Xử lý lỗi nếu có
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+        }
     } catch (error) {
-        console.log(error)
+        console.error(error);
     }
 }
+
+export const EditStudent = async (user,studentId) => {
+    try {
+        const response = await fetch(`${baseUrl}/${studentId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+
+        if (response.ok) {
+            console.log('User updated successfully');
+        } else {
+            // Xử lý lỗi nếu có
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getAllStudents = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/`, { 
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json(); // Lấy dữ liệu từ phản hồi
+            console.log('Get Students successfully');
+            return data; // Trả về dữ liệu nếu cần
+        } else {
+            // Xử lý lỗi nếu có
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+            throw new Error(errorMessage); // Ném ra lỗi nếu có lỗi
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Ném ra lỗi trong trường hợp xảy ra lỗi trong khối try-catch
+    }
+};
+
+export const deleteStudent = async (studentId) => {
+    try {
+        const response = await fetch(`${baseUrl}/${studentId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            console.log('Student deleted successfully');
+        } else {
+            // Xử lý lỗi nếu có
+            const errorMessage = await response.text();
+            console.log(errorMessage);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
--- AwesomeProject/src/Components/AddStudents/addStudens.jsx
+++ AwesomeProject/src/Components/AddStudents/addStudens.jsx
@@ -1,38 +1,204 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import Input from './../Input/input';
-import { createStudent } from '../../API/apiRequest';
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { EditStudent, createStudent, deleteStudent, getAllStudents } from '../../API/apiRequest';
 
 const AddStudent = () => {
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
-    const [className, setClassName] = useState('');
-    const [gpa, setGPA] = useState(null);
+    const [Class, setClassName] = useState('');
+    const [gpa, setGPA] = useState('');
+    const [students, setStudents] = useState([]);
+    const [editMode, setEditMode] = useState({});
+
+    // Hàm này sẽ gọi danh sách sinh viên từ cơ sở dữ liệu khi component được tải lần đầu
+    useEffect(() => {
+      getAllStudents()
+        .then((data) => {
+          setStudents(data);
+          // Khởi tạo trạng thái chỉnh sửa cho mỗi sinh viên thành false ban đầu
+          const initialEditMode = {};
+          data.forEach((student) => {
+            initialEditMode[student.id] = false;
+          });
+          setEditMode(initialEditMode);
+        })
+        .catch((error) => {
+          console.error('Error fetching students:', error);
+        });
+    }, []);
 
     const saveStudentInfo = () => {
-        const user = {
+        const newUser = {
             name,
             address,
-            className,
+            Class,
             gpa,
         };
-        console.log(user)
-        try {
-            createStudent(user); // Gọi hàm createStudent từ API
-            console.log('Student created successfully');
-        } catch (err) {
-            console.log('Error creating student:', err);
-        }
+        createStudent(newUser)
+            .then(() => {
+                console.log('Student created successfully');
+                // Sau khi thêm sinh viên, gọi lại danh sách để cập nhật
+                refreshStudentList();
+            })
+            .catch((err) => {
+                console.log('Error creating student:', err);
+            });
+    }
+
+    const handleEdit = (id) => {
+        const newUser = {
+            name,
+            address,
+            Class,
+            gpa,
+        };
+        EditStudent(newUser, id)
+            .then(() => {
+                // console.log('Student updated successfully');
+                // Sau khi chỉnh sửa sinh viên, gọi lại danh sách để cập nhật
+                refreshStudentList();
+            })
+            .catch((err) => {
+                console.log('Error updating student:', err);
+            });
+    }
+
+    const handleDelete = (id) => {
+        deleteStudent(id)
+            .then(() => {
+                console.log("Delete Successfully!");
+                // Sau khi xóa sinh viên, gọi lại danh sách để cập nhật
+                refreshStudentList();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    const refreshStudentList = () => {
+        getAllStudents()
+            .then((data) => {
+                setStudents(data);
+                // Khởi tạo trạng thái chỉnh sửa cho mỗi sinh viên thành false
+                const updatedEditMode = {};
+                data.forEach((student) => {
+                    updatedEditMode[student.id] = false;
+                });
+                setEditMode(updatedEditMode);
+            })
+            .catch((error) => {
+                console.error('Error fetching students:', error);
+            });
     }
 
     return (
         <View style={styles.container}>
-            <Input label="Name" placeholder="Enter your name" onChangeText={setName} />
-            <Input label="Address" placeholder="Enter your address" onChangeText={setAddress} />
-            <Input label="Class" placeholder="Enter your class" onChangeText={setClassName} />
-            <Input label="GPA" placeholder="Enter your GPA" keyboardType="numeric" onChangeText={setGPA} />
+            <Text>Name: </Text>
+            <TextInput
+                style={styles.input}
+                label="Name"
+                placeholder="Enter your name"
+                onChangeText={(value) => setName(value)}
+            />
+            <Text>Address: </Text>
+            <TextInput
+                style={styles.input}
+                label="Address"
+                placeholder="Enter your address"
+                onChangeText={(value) => setAddress(value)}
+            />
+            <Text>ClassName: </Text>
+            <TextInput
+                style={styles.input}
+                label="Class"
+                placeholder="Enter your class"
+                onChangeText={(value) => setClassName(value)}
+            />
+            <Text>GPA: </Text>
+            <TextInput
+                style={styles.input}
+                label="GPA"
+                placeholder="Enter your GPA"
+                keyboardType="numeric"
+                onChangeText={(value) => setGPA(value)}
+            />
 
             <Button title="Add Student" onPress={saveStudentInfo} />
+            <View style={styles.title}>
+                <Text style={styles.title}>Students List: </Text>
+            </View>
+
+            <FlatList
+                data={students}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={stylesFlatList.container}>
+                        {editMode[item.id] ? (
+                            // Hiển thị trường nhập liệu khi chỉnh sửa
+                            <View>
+                                <Text>StudentID: {item.id}</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your name"
+                                    value={name}
+                                    onChangeText={(value) => setName(value)}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your address"
+                                    value={address}
+                                    onChangeText={(value) => setAddress(value)}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your class"
+                                    value={Class}
+                                    onChangeText={(value) => setClassName(value)}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="Enter your gpa"
+                                    value={gpa}
+                                    onChangeText={(value) => setGPA(value)}
+                                />
+                            </View>
+                        ) : (
+                            // Hiển thị thông tin khi không chỉnh sửa
+                            <View>
+                                <Text>StudentID: {item.id}</Text>
+                                <Text>Name: {item.name}</Text>
+                                <Text>Address: {item.address}</Text>
+                                <Text>Class: {item.class}</Text>
+                                <Text>GPA: {item.gpa}</Text>
+                            </View>
+                        )}
+                        <View style={styles.button}>
+                            {editMode[item.id] ? (
+                                // Hiển thị nút "Save" khi chỉnh sửa
+                                <Button
+                                    title="Save"
+                                    onPress={() => {
+                                        handleEdit(item.id);
+                                        setEditMode({ ...editMode, [item.id]: false });
+                                    }}
+                                />
+                            ) : (
+                                // Hiển thị nút "Edit" khi không chỉnh sửa
+                                <Button
+                                    title="Edit"
+                                    onPress={() => setEditMode({ ...editMode, [item.id]: true })}
+                                />
+                            )}
+                        </View>
+                        <View style={styles.button}>
+                            <Button
+                                title="Delete"
+                                onPress={() => handleDelete(item.id)}
+                            />
+                        </View>
+                    </View>
+                )}
+            />
         </View>
     );
 }
@@ -41,6 +207,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+        marginTop: 20,
+        paddingBottom: 20,
+        marginBottom: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        padding: 2,
+        paddingLeft: 10,
+        marginBottom: 10,
+    },
+    button: {
+        color: 'red',
+        padding: 5,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        paddingTop: 5,
+    },
+});
+
+const stylesFlatList = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
     },
 });
 
